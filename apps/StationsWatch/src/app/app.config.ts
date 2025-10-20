69abc9c3bb00bef81ee5f14034d1c5065b4e5285
Marc <webdev@marcbakker.com>
2025-10-20 15:31:09 +0200

    Omgevingsvariabelen toevoegen voor ns endpoint en api key.

--- apps/StationsWatch/src/app/app.config.ts
+++ apps/StationsWatch/src/app/app.config.ts
@@ -5,11 +5,22 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { NS_STATIONS_ENDPOINT, OCP_APIM_SUBSCRIPTION_KEY } from './environments/.environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(),
+    {
+      provide: NS_STATIONS_ENDPOINT,
+      useValue: NS_STATIONS_ENDPOINT
+    },
+    {
+      provide: OCP_APIM_SUBSCRIPTION_KEY,
+      useValue: OCP_APIM_SUBSCRIPTION_KEY
+    }
   ],
 };
---
+++ apps/StationsWatch/src/app/environments/.environment.ts
@@ -0,0 +1,2 @@
+export const NS_STATIONS_ENDPOINT = '/nsapp-stations/v2';
+export const OCP_APIM_SUBSCRIPTION_KEY = '1ea3dd385baf4127a20cb8fb38af634d';
