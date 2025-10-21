a55c431797fdd0adc89a0aa3185af5360563fa4c
Marc <webdev@marcbakker.com>
2025-10-21 20:33:48 +0200

    Vervangen code voor storing card met component

--- apps/StationsWatch/src/app/app.ts
+++ apps/StationsWatch/src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, inject, ViewEncapsulation, OnInit, Signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CustomList } from '../../../../libs/components';
+import { StoringCard } from '../../../../libs/components';
 import { NsApiService } from 'libs/api';
 import { Station } from 'libs/models';
 import { Observable } from 'rxjs';
 import { toSignal} from '@angular/core/rxjs-interop';
-import { MatCardModule } from '@angular/material/card';
 
 @Component({
-  imports: [RouterModule, CustomList, MatCardModule],
+  imports: [RouterModule, CustomList, StoringCard],
   selector: 'ns-root',
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
