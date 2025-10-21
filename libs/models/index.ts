a02b9a7db0adda0f3ee09f6656075b2b0bd17db8
Marc <webdev@marcbakker.com>
2025-10-21 14:01:05 +0200

    verwijder apps door NX standaard toegevoegd

--- libs/models/index.ts
+++ libs/models/index.ts
@@ -1,4 +1,3 @@
-export * from './src/lib/product.model';
 export type { StationData } from './src/lib/station.data.model';
 export type { StationUitgebreid } from './src/lib/station.data.model';
 export type { Station } from './src/lib/station.model';
