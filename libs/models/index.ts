80600ebf375551f3a111774b0c1f0dfea0d7ff23
Marc <webdev@marcbakker.com>
2025-10-22 16:55:15 +0200

    moet nog bij commit "Alle stations models in een bestand ipv 1 bestand per model."

--- libs/models/index.ts
+++ libs/models/index.ts
@@ -1,3 +1,5 @@
-export type { StationData } from './src/lib/station.data.model';
-export type { StationUitgebreid } from './src/lib/station.data.model';
-export type { Station } from './src/lib/station.model';
+export type { StationData } from './src/lib/station.models';
+export type { StationUitgebreid } from './src/lib/station.models';
+export type { Station } from './src/lib/station.models';
+export type { Landcode } from './src/lib/station.models';
+export type { StationsStoring } from './src/lib/station.models';
