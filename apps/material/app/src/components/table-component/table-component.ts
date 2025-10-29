d9e292382b778a930917bb27a0acc1f8612f8872
Marc <webdev@marcbakker.com>
2025-10-29 21:59:56 +0100

    initial commit table-component

---
+++ apps/material/app/src/components/table-component/table-component.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-table-component',
+  imports: [],
+  templateUrl: './table-component.html',
+  styleUrl: './table-component.scss',
+  encapsulation: ViewEncapsulation.Emulated,
+})
+export class TableComponent {}
