d46b9f5ebb7cf0a765f4b22be9de429bf62b9fad
Marc <webdev@marcbakker.com>
2026-02-05 18:37:08 +0100

    NS heeft de Api key gewijzigd

--- apps/StationsWatch/src/app/environments/.environment.ts
+++ apps/StationsWatch/src/app/environments/.environment.ts
@@ -1,2 +1,2 @@
 export const NS_STATIONS_ENDPOINT = '/nsapp-stations/v2';
-export const OCP_APIM_SUBSCRIPTION_KEY = '1ea3dd385baf4127a20cb8fb38af634d';
+export const OCP_APIM_SUBSCRIPTION_KEY = '3833ed4cbc5d43bd9241420caf04365c';
